731621bceef947de8bbb2fec9a249257d1defb46
TaraRhoseyn <taragulwell@gmail.com>
2021-11-18 15:42:41 +0000

    Add gulp task to render nunjucks views

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,10 @@
 const gulp = require('gulp');
-const nunjucksRender = require('gulp-nunjucks-render');
+const render = require('gulp-nunjucks-render');
+
+gulp.task('nunjucks', function() {
+  return gulp.src('src/pages/*')
+  .pipe(render({
+      path: ['src/templates']
+    }))
+  .pipe(gulp.dest('public'))
+});
